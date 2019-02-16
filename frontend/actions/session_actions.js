56808ff2be553a999d6c4a0f669d3b2f063c645c
Daniel Moon <moona3k@gmail.com>
2019-02-16 01:47:55 -0800

    add session_form

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,52 @@
+import * as APIUtil from '../util/session';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+// regular action creator
+const receiveCurrentUser = user => {
+    return ({
+        type: RECEIVE_CURRENT_USER,
+        user: user
+    });
+};
+
+const logoutCurrentUser = () => {
+    return ({
+        type: LOGOUT_CURRENT_USER
+    });
+};
+
+const receiveErrors = errors => {
+    return ({
+        type: RECEIVE_SESSION_ERRORS,
+        errors: errors
+    });
+};
+
+// thunk action creator
+
+// create new user
+// first arg 'formUser' is the data object from submitting form
+// second arg 'dispatch' will be received from middleware
+export const signup = formUser => dispatch => {
+    return APIUtil.postUser(formUser)
+        .then(user => dispatch(receiveCurrentUser(user))
+        ), err => (dispatch(receiveErrors(err.responseJSON))
+        );
+}
+
+// login user
+export const login = formUser => dispatch => {
+    return APIUtil.postSession(formUser)
+        .then(user => dispatch(receiveCurrentUser(user))
+        ), err => (dispatch(receiveErrors(err.responseJSON))
+        );
+}
+
+// logout user
+export const logout = () => dispatch => {
+    return APIUtil.deleteSession()
+        .then(() => dispatch(logoutCurrentUser()));
+}
