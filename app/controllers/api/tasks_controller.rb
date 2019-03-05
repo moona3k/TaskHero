16ca2e73b9d6e73339b0f13e6abbf9e122051610
Daniel Moon <moona3k@gmail.com>
2019-03-04 17:08:46 -0800

    add create action controller for 'tasks'

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,6 +1,19 @@
 class Api::TasksController < ApplicationController
 
     def create
+        @task = Task.new(task_params)
+
+        if @task.save
+            render json: @task 
+        else
+            render json: @task.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:task_category, :location, :description, :user_id, :tasker_id, :estimated_time_req, :scheduled_time, :scheduled_date, :is_complete, :specific_location, :vehicle_type)
     end
 
 end
